c4e2bb5c4e73e3476b42ee9f948f7097cba6689b
Michael Wegner <Michael.Wegner@supplyon.com>
2019-01-14 08:23:33 +0100

    added installation of zsh to install script

--- zsh/first_time_install.sh
+++ zsh/first_time_install.sh
@@ -3,7 +3,7 @@
 
 # install power line fonts
 sudo apt-get update
-sudo apt-get install fonts-powerline
+sudo apt-get install fonts-powerline zsh
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
